e60f270933485b3f8f2c6201eb422c95244d259f
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2020-04-26 21:58:06 +0200

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+from math import *
+from collections import *
+
+#Calculate Factorial
+x=input("Enter a positive integer: ")
+y=int(x)
+print("The entered number is", y)
+a=factorial(y)
+print("Factorial of",y, "is" ,a)
